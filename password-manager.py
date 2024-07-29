f5adf6ce60d7d5dd4ebf37bd8ab2c15a4f947499
Amirreza Nazemi <amirrnazemi@lines.so>
2024-07-29 23:09:32 +0330

    Add password generator and strength checker function skeletons
    - Imported necessary modules: string, shuffle, choice from random, and Union from typing.
    - Added shuffled lists for lowercase letters, uppercase letters, digits, and special characters.
    - Defined `password_generator` function skeleton to generate a password with specified character number and included character types, with an optional password strength check.
    - Defined `check_password_strength` function skeleton to check the strength of a given password, with an optional return type.

---
+++ password-manager.py
@@ -0,0 +1,26 @@
+import string
+
+from random import shuffle, choice
+from typing import Union
+
+# # لیست حروف کوچک انگلیسی به صورت به‌ هم‌ ریخته
+# lowercase_letters = list(string.ascii_lowercase)
+# shuffle(lowercase_letters)
+
+# # لیست حروف بزرگ انگلیسی به صورت به‌ هم‌ ریخته
+# uppercase_letters = list(string.ascii_uppercase)
+# shuffle(uppercase_letters)
+
+# # لیست اعداد به صورت به‌ هم‌ ریخته
+# digits = list(string.digits)
+# shuffle(digits)
+
+# # لیست کاراکترهای خاص به صورت به‌ هم‌ ریخته
+# special_characters = list(string.punctuation)
+# shuffle(special_characters)
+
+def password_generator(char_num: int, includes: list, check_strength: bool = False)->Union[str, list]:
+    pass
+
+def check_password_strength(password: str, return_type: bool = True)->Union[str, int]:
+    pass
