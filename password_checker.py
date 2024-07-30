52bd727cd0eb940a4520dae0397ef90b558e65c8
Amirreza Nazemi <amirrnazemi@lines.so>
2024-07-30 10:27:32 +0330

    نوشتن مرحله اول تابع بررسی کننده پسورد
    در این مرحله فقط به نحوه برچسب گذاری و برچسب ها پرداخته شد و امتیاز دهی هدف نبوده است.

--- password_checker.py
+++ password_checker.py
@@ -4,5 +4,19 @@ from random import shuffle, choice
 from typing import Union
 
 
-def check_password_strength(password: str, return_type: bool = True)->Union[str, int]:
-    pass
+def check_password_strength(password: str, tagged: bool = True)->Union[str, int]:
+
+    password_point = 0
+    tags = {90: 'very strong', 75: 'strong', 50: 'medium', 25: 'weak', 0: 'very weak'}
+    
+    password_point = len(password) * 5
+    
+    password_point = max(min(password_point, 100), 0)
+    
+    if tagged:
+        for min, tag in tags:
+            if password_point >= min:
+                return tag
+        return tags[0]
+    else:
+        return password_point
