cfbe1df565d63c4b8b63aa75d7644d4adc1f09a6
FatemeRajabii <Fatemessfr@gmail.com>
2024-08-02 19:10:51 +0330

    second debug

--- password_generator.py
+++ password_generator.py
@@ -1,18 +1,19 @@
 import random
 import string
-
+import password_checker
 
 def password_generator(char_num:int,includes:list=[1,2,3,4],check_strength:bool=False):
-    #includes ro tabdil be ye list az char haye entekhabi kardam , masalan:all_chars=[["a","b",..],["A","B",..],..]
+    
     all_chars=[]
 
-    #is it necessary?
     if char_num == 0:
         if check_strength:
             return ['', None]
         else:
             return ''
 
+    if type(includes)!=list or not len(includes):
+        includes=[1,2,3,4]
 
     if 1 in includes:
         all_chars.extend(list(string.ascii_lowercase))
@@ -25,14 +26,15 @@ def password_generator(char_num:int,includes:list=[1,2,3,4],check_strength:bool=
     
 
     random.shuffle(all_chars)
-    password="".join(all_chars[5:char_num+5])
 
-    import password_checker
+    password_list=[]
+    for i in range(char_num):
+        password_list.append(random.choice(all_chars))
+    password="".join(password_list)
+
+
     
     if check_strength:
         return[password,password_checker.check_password_strength(password)]
     else:
         return password
-    
-    
-print(password_generator(10,check_strength=False))
